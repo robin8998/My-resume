9aa9c250a135f0229a69e82accbd19d7ddf51a72
Robin Singh <ranarobin8998@gmail.com>
2025-04-04 19:09:24 +0530

    create navItems for header component

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Header() {
+    const [currentItem,setCurrentItem] = useState("Home")
+
+    const navItems = [
+        {
+            name : "Robin Singh/Frontend Developer",
+            slug : "/" ,
+            isCurrent : currentItem
+        },
+
+        {
+            name : "Resume",
+            slug : "/resume",
+            isCurrent : currentItem
+        },
+
+        {
+            name : "Projects",
+            slug : "/projects",
+            isCurrent : currentItem
+        },
+
+        {
+            name : "Contact",
+            slug : "/contact",
+            isCurrent : currentItem
+        },
+    ]
   return (
     <div>
-        
+       
     </div>
   )
 }
