08b5c0179ba458734fed9459acaac0116c451de4
Robin Singh <ranarobin8998@gmail.com>
2025-04-05 11:47:17 +0530

    create navigation in header component

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from 'react'
+import {useNavigate} from "react-router-dom"
 
 function Header() {
     const [currentItem,setCurrentItem] = useState("Home")
+    const navigate = useNavigate()
 
     const navItems = [
-        {
-            name : "Robin Singh/Frontend Developer",
-            slug : "/" ,
-            isCurrent : currentItem
-        },
 
         {
             name : "Resume",
@@ -28,9 +25,24 @@ function Header() {
             isCurrent : currentItem
         },
     ]
+
+    const currentSection = (item) =>{    // to highlight the current section in navbar 
+     navigate(item.slug)    
+     setCurrentItem(item.name)
+    }
   return (
     <div>
-       
+     <header>
+        <nav>
+            {navItems.map((item)=>
+            (
+                <li>
+                    <button>{item.name}</button>
+                </li>
+            )
+            )}
+        </nav>
+     </header>
     </div>
   )
 }
