28d74d89962c4081f371a71b8bba9ef2be492110
Robin Singh <ranarobin8998@gmail.com>
2025-04-06 16:34:47 +0530

    modify and inject component inside App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import { Outlet } from 'react-router-dom'
+import Header from './header/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <h1>My resume </h1>
-    </>
+    <div>
+      <div>
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+      </div>
+    </div>
   )
 }
 
