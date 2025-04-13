d88fa6720ee6e4922cc7d8fd262e4093fcf3d3ad
Robin Singh <ranarobin8998@gmail.com>
2025-04-13 17:55:08 +0530

    add details to projectCard

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 function projectCard(project){
   return (
     <div>
-        <h1>name</h1> : <p>{project.name}</p>
+        <h1>Name</h1> : <p>{project.Name}</p>
         <h1>Category</h1> : <p>{project.Category}</p>
-        <h1>description</h1> : <p>{project.description}</p>
+        <h1>Description</h1> : <p>{project.Description}</p>
+        <h1>Code</h1> : <p>{project.Code}</p>
+        <h1>LiveLink</h1> : <p>{project.LiveLink}</p>
+        <h1>Tech</h1> : <p>{project.Tech}</p>
+        <h1>Features</h1> : <p>{project.Features}</p>
+        <h1>Image</h1> : <p>{project.Image}</p>
     </div>
   )
 }
