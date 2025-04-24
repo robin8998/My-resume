be6ecacc16bb6309526170f93c32ee0927331a05
Robin Singh <ranarobin8998@gmail.com>
2025-04-24 23:12:22 +0530

    add new files

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Header from './components/header/Header'
 import { About, Contact, Projects,  } from './components'
 import MyBg from './components/MyBg'
 import Myname from './components/Myname'
+import Skills from './components/pages/Skills'
 
 function App() {
   
   return (
-    <div>
       <div>
         <Header/>
         <main>
@@ -14,10 +14,10 @@ function App() {
           <Myname/>
           <About/>
           <Projects/>
+          <Skills/>
           <Contact/>
         </main>
       </div>
-    </div>
   )
 }
 
