8f38bbb22f0dc778af082c7899bb4edd9e40ad71
Robin Singh <ranarobin8998@gmail.com>
2025-04-24 23:09:41 +0530

    upate

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,34 +1,21 @@
-import { useState, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-
-
 function Header() {
-  const [currentItem, setCurrentItem] = useState("Home")
-  const [isOpen, setIsOpen] = useState(false)
-  const [scrolled, setScrolled] = useState(false)
-  const navigate = useNavigate()
-
-  const navItems = [
-    { name: "Resume", slug: "/resume",  },
-    { name: "Projects", slug: "/projects", },
-    { name: "Contact", slug: "/contact", },
-  ]
-
-  // Handle scroll effect for header
- 
-
-   
   return (
-    <nav className="flex justify-between px-4 py-4  bg-[#13161B] items-center sticky top-0 z-50">
-      <div className="font-[700] cursor-pointer">
+    <nav className="flex flex-col md:flex-row text-xl justify-between items-center px-4 py-4 bg-[#13161B] sticky top-0 z-50 space-y-2 md:space-y-0">
+      <div className="font-[700] cursor-pointer text-center md:text-left">
         <span className="text-blue-400">Robin </span>Singh
       </div>
 
       <div>
-        <span className="font-[700] cursor-pointer">Download CV</span>
+        <a
+          href="/pdf/resume.pdf"
+          download
+          className="font-[700] cursor-pointer text-white no-underline hover:text-blue-400 visited:text-white"
+        >
+          Download CV
+        </a>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Header
