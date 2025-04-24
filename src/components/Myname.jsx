b274d8d7162c400a02cc7f0e11a8a34dc270ed64
Robin Singh <ranarobin8998@gmail.com>
2025-04-24 23:09:21 +0530

    update

--- src/components/Myname.jsx
+++ src/components/Myname.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Myname = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center text-6xl">
+    <div className="w-full h-screen flex justify-center items-center sm:text-xl md:text-6xl">
       <h1 className="font-Kanit font-medium text-amber-50">
         Robin Singh
       </h1>
