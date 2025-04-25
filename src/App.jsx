44c32952cf4a5fbed7aab50e37b223a14f26fce2
Robin Singh <ranarobin8998@gmail.com>
2025-04-25 08:36:40 +0530

    update settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Header from './components/header/Header'
-import { About, Contact, Projects,  } from './components'
+import { About, Contact, Projects,  } from './components/index'
 import MyBg from './components/MyBg'
 import Myname from './components/Myname'
 import Skills from './components/pages/Skills'
 
 function App() {
-  
+    
   return (
       <div>
         <Header/>
--- src/main.jsx
+++ src/main.jsx
@@ -2,38 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-import { About,Contact,Projects,Resume } from './components/index.js'
-
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element : <App/>,
-    children : [
-      {
-        path : "/",
-        element : <About/>
-      },
-      {
-        path : "/contact",
-        element : <Contact/>
-      },
-      {
-        path : "/projects",
-        element: <Projects/>
-      },
-      {
-        path: "/resume",
-        element : <Resume/>
-      }
-    ]
-  }
-])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={routes}>
+  <StrictMode>    
     <App />
-    </RouterProvider>
   </StrictMode>,
 )
