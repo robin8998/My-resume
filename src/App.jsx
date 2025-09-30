c3e72f038bd321f2e7845e0a90013e438d02d957
Robin Singh <ranarobin8998@gmail.com>
2025-09-30 08:14:25 +0530

    add css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 import { Outlet } from 'react-router-dom'
-
+import Header from './components/header/Header'
+import { About, Contact, Projects,  } from './components'
+import MyBg from './components/MyBg'
+import Myname from './components/Myname'
+import Skills from './components/pages/Skills'
 
 function App() {
   
   return (
     <div>
       <div className='overflow-hidden'>
+        <Header/>
         <main>
-          <h1 className='text-white'>My portfolio</h1>
+          <MyBg/>
+          <Myname/>
+          <About/>
+          <Skills/>
+          <Projects/>
+          <Contact/>
         </main>
       </div>
     </div>
