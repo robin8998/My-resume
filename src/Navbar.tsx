93552e0d7945868a72a0e814704e75a987557c65
Robin Singh <ranarobin8998@gmail.com>
2025-10-01 16:19:41 +0530

    configure typescript

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/resizable-navbar";
 import { useState } from "react";
  
-export function NavbarOrg() {
+export function NavbarDemo() {
   const navItems = [
     {
       name: "Features",
