ea116fc92182d5c79ba6da549042309e36e9dbcf
Robin Singh <ranarobin8998@gmail.com>
2025-10-02 16:51:51 +0530

    rename js type to ts

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { NavbarOrg } from "./Navbar"
+
+function App() {
+    
+  return (
+    <div className="bg-gray-600 w-full min-h-screen">
+        <NavbarOrg/>
+        <main>
+          <h1 className='text-black'>My portfolio</h1>
+        </main>
+      </div>
+  )
+}
+
+export default App
