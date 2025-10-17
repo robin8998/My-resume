ea6dadc48337e2bbbda1de868f535b10eb70db18
Robin Singh <ranarobin8998@gmail.com>
2025-10-17 16:36:50 +0530

    modify skill section

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -37,7 +37,7 @@ function Skills() {
     <div className="text-white text-center text-5xl w-full space-y-4 mt-10 flex flex-col px-10 py-20">
       SKILLS TO OFFER
 
-      <div className="h-[40rem] rounded-md flex flex-col antialiased bg-black dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
+      <div className="h-[25rem] rounded-md flex flex-col antialiased bg-black dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
         <InfiniteMovingIcons
           items={skills}
           direction="right"
