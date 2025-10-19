46228904a774beac8869a6574b325865e503ea37
Robin Singh <ranarobin8998@gmail.com>
2025-10-19 17:01:14 +0530

    use project images

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,49 +3,64 @@ import { StickyScroll } from "@/components/ui/sticky-scroll-reveal"
 function Projects() {
   const content = [
   {
-    title: "Collaborative Editing",
+    title: "Home Page",
     description:
       "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
     content: (
-      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
-        Collaborative Editing
-      </div>
+      <div className="flex h-full w-full items-center justify-center text-white">
+        <img 
+        src="/images/Blog/Home.png" 
+        width={300}
+        height={300}
+        className="h-full w-full object-cover"
+        alt="Home Image" />
+      </div>  
     ),
   },
   {
-    title: "Real time changes",
+    title: "Post View",
     description:
       "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
     content: (
       <div className="flex h-full w-full items-center justify-center text-white">
         <img
-          src="/linear.webp"
+          src="/images/Blog/Post.png"
           width={300}
           height={300}
           className="h-full w-full object-cover"
-          alt="linear board demo"
+          alt="Post View Image"
         />
       </div>
     ),
   },
-  {
-    title: "Version control",
+   {
+    title: "Add Post",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
     content: (
-      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] text-white">
-        Version control
-      </div>
+      <div className="flex h-full w-full items-center justify-center text-white">
+        <img 
+        src="/images/Blog/newPost.png" 
+        width={300}
+        height={300}
+        className="h-full w-full object-cover"
+        alt="Add Post Image" />
+      </div>  
     ),
   },
-  {
-    title: "Running out of content",
+   {
+    title: "Home Page",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
     content: (
-      <div className="flex h-full w-full items-center justify-center bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] text-white">
-        Running out of content
-      </div>
+      <div className="flex h-full w-full items-center justify-center text-white">
+        <img 
+        src="/images/Blog/Signup.png" 
+        width={300}
+        height={300}
+        className="h-full w-full object-cover"
+        alt="Signup Page" />
+      </div>  
     ),
   },
 ];
